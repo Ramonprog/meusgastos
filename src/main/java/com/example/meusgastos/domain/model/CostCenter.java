438cbf01c8a9423494fcc38ba9a18cd9c15d5ce7
RamonProg <programador.ramon@gmail.com>
2026-05-01 19:34:24 -0300

    feat: criando tabela de centro de custo

---
+++ src/main/java/com/example/meusgastos/domain/model/CostCenter.java
@@ -0,0 +1,36 @@
+package com.example.meusgastos.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cost_center")
+public class CostCenter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(columnDefinition = "TEXT")
+    private String observation;
+
+    @ManyToOne()
+    @JoinColumn(name = "id")
+    private Usuario usuario;
+
+    // aqui temos que colocar a tabela mandatoria
+    @ManyToMany(mappedBy = "cost_center")
+    @JsonBackReference // quando buscar um centro de custo, não vai trazer os titulos
+    private Titles titles;
+}
--- src/main/java/com/example/meusgastos/domain/model/Titles.java
+++ src/main/java/com/example/meusgastos/domain/model/Titles.java
@@ -1,5 +1,15 @@
 package com.example.meusgastos.domain.model;
 
-public class Titles {
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+public class Titles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
 }
